34d4514eae85fa8c0b3f81b31d95c141b82df0d3
joa-dev <joa.neis.dev@gmail.com>
2023-02-08 22:56:59 -0300

    WIP2

--- frontend/screens/Signin.js
+++ frontend/screens/Signin.js
@@ -14,48 +14,57 @@ const Signin = () => {
   return (
 	<SafeAreaView style={styles.container}>
 	<View>
-	  <Text>Bienvenido</Text>
+	  <Text style={styles.welcome}>Bienvenido</Text>
 	  <Text>Email</Text>
 	  <TextInput
-        style={styles.input}
+        style={styles.email}
         onChangeText={onChangeEmail}
         value={email}
         placeholder="📧 Escribe Tu Correo Electrónico"
         keyboardType="email-address"
       />
+	  <Text>Contraseña</Text>
 	  <TextInput
-        style={styles.input}
+        style={styles.password}
+		secureTextEntry={true}
         onChangeText={onChangePassword}
         value={password}
         placeholder="🔒 Escribe Tu Contraseña"
         keyboardType="default"
       />
-	  <Text>Olvidaste tu contraseña</Text>
-	  <Button
+	  <Text style={styles.forgotPassword}>Olvidaste tu contraseña</Text>
+	  <View style={styles.viewSignin}>
+	  	<Button
 	  	title="Iniciar Sesión"
-		color= "#198E6B"
+		color='#198E6B'
 		onPress={() => Alert.alert('Iniciaste Sesión')}
-	  />
-	  <Text>Conectar con</Text>
-	  <Button
-	  	title= "Google"
-		onPress={() => Alert.alert('Te logueaste con Google')}
-	  />
-	  <Button
-	  	title="Facebook"
-		onPress={() => Alert.alert('Te logueaste con Facebook')}
-	  />
-	  <Text>No tienes una cuenta?</Text>
-	  <Text>Crear</Text>
+	  	/>
+	  </View>
+	  <Text style={styles.viewText}>Conectar con</Text>
+	  <View style={styles.viewNetworks}>
+		  <View style={styles.viewGoogle}>
+		  	<Button
+		  		title= "Google"
+				onPress={() => Alert.alert('Te logueaste con Google')}
+		  	/>
+		  </View>
+		  <View style={styles.viewFacebook}>
+		  	<Button
+		  		title="Facebook"
+				onPress={() => Alert.alert('Te logueaste con Facebook')}
+		  	/>
+		  </View>
+	  </View>
+	  <View style={styles.viewText}>
+	  	<Text>No tienes una cuenta?</Text>
+	  	<Text>Crear</Text>
+	  </View>
 	</View>
 	</SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-	Button:{
-		color: '#6FDDA7'
-	},
 	container: {
 	  flex: 1,
 	  justifyContent: 'center',
@@ -64,7 +73,6 @@ const styles = StyleSheet.create({
 	title: {
 	  textAlign: 'center',
 	  marginVertical: 8,
-	  color: '#6FDDA7',
 	},
 	fixToText: {
 	  flexDirection: 'row',
@@ -75,13 +83,57 @@ const styles = StyleSheet.create({
 	  borderBottomColor: '#737373',
 	  borderBottomWidth: StyleSheet.hairlineWidth,
 	},
-	input: {
-		height: 40,
+	welcome: {
+		fontWeight: 'bold',
+		textAlign: 'center',
+		fontSize: 20,
+		margin: 70,
+	},
+	email: {
+		height: 50,
 		margin: 12,
-		borderWidth: 0,
 		padding: 10,
+		borderWidth: 0,
 		backgroundColor: '#6FDDA7',
+		borderRadius: 20,
 	  },
+	password: {
+		height: 50,
+		margin: 12,
+		padding: 10,
+		borderWidth: 1,
+		borderColor: '#000000',
+		borderRadius: 20,
+	  },
+	viewSignin: {
+		height: 50,
+		margin: 12,
+		borderRadius: 20,
+		overflow: 'hidden',
+	  },
+	viewFacebook: {
+		borderRadius: 20,
+		overflow: 'hidden',
+	  },
+	viewGoogle: {
+		borderRadius: 20,
+		overflow: 'hidden',
+	  },
+	viewNetworks: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginBottom: 50,
+		marginHorizontal: 20,
+	},
+	viewText: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		textAlign: 'center',
+		margin: 20,
+	},
+	forgotPassword: {
+		textAlign: 'right',
+	}
   });
 
 export default Signin
