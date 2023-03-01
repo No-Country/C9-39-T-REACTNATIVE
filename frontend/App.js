53239bba1dd6f9b21da3b2f88083b868f44de479
unknown <gipajfm@gmail.com>
2023-03-01 15:43:00 -0500

    problema de la parte superior de celular no se muestra el reloj - solucionado

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { AuthProvider } from './global/globalVar'
 import AppRoutes from './routes/AppRoutes'
+import { View,StyleSheet } from "react-native";
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
+      <View
+      style={styles.container}
+      >
+        <AppRoutes />
+      </View>
     </AuthProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight
+
+  }
+});
